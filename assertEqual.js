a627ee3291f24de164c27a74301c5ff462d3e502
Calvin <calvinchan87@gmail.com>
2021-03-31 16:24:44 +0000

    remove test code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,3 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Matthew", "Matthew");
-assertEqual(1, 2);
