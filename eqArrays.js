f661b8479c112a62297094a1d9746942be572531
Calvin <calvinchan87@gmail.com>
2021-03-31 16:12:20 +0000

    take in two arrays, test for perfect match

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let x = 0; x < arrayOne.length; x++) {
+    if (arrayOne[x] !== arrayTwo[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// TEST CODE
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
