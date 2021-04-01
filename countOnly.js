833631f0f19711188e1f37fab8c8dc14680638a0
Calvin <calvinchan87@gmail.com>
2021-04-01 16:05:03 +0000

    fix minor eslint errors

--- countOnly.js
+++ countOnly.js
@@ -16,14 +16,14 @@ const countOnly = function(allItems, itemsToCount) {
     if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
-     } else {
-       results[item] = 1;
+      } else {
+        results[item] = 1;
       }
     }
   }
 
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
