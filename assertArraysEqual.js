e98693014c9ee945d850abb24c7cefa4c3a26380
Calvin <calvinchan87@gmail.com>
2021-04-01 00:15:50 +0000

    refactored assertArraysEqual function to use eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -18,3 +18,12 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
   return true;
 };
+
+
+// TEST CODE
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
