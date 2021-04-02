32e161aa8a0c4ff8bccb76156a43f363abd4d565
Calvin <calvinchan87@gmail.com>
2021-04-02 14:54:04 +0000

    Implement the Map function

---
+++ map.js
@@ -0,0 +1,44 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const results2 = map(words, word => word[1]);
+console.log(results2);
+
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let x = 0; x < arrayOne.length; x++) {
+    if (arrayOne[x] !== arrayTwo[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// TEST CODE
+
+assertArraysEqual(eqArrays(results1, ["g", "c", "t", "m", "t"]), true); // => true
+assertArraysEqual(eqArrays(results1, ["r", "o", "o", "a", "o"]), false); // => true
+assertArraysEqual(eqArrays(results2, ["r", "o", "o", "a", "o"]), true); // => true
