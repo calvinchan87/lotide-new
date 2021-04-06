57ea53f17a86c7af45b51676f84ac1c6a0c3d2c7
Calvin <calvinchan87@gmail.com>
2021-04-06 00:58:23 +0000

    modularize/refactor assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -7,23 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let x = 0; x < arrayOne.length; x++) {
-    if (arrayOne[x] !== arrayTwo[x]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-// TEST CODE
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = assertArraysEqual;
